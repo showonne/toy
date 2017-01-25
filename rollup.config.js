9d5f13f321416bdcabecb6845502f20dc9c12569
陈景辉 <918677896@qq.com>
2017-01-25 15:52:40 +0800

    ch build method

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
+import process from 'process'
+
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+
+let plugins = []
+
+plugins.push(babel({
+            exclude: 'node_modules/**'
+        }))
+
+if(process.env.BUILD === 'production'){
+    plugins.push(uglify())
+}
 
 export default {
     entry: 'src/index.js',
     dest: 'dest/toy.js',
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        })
-    ]
+    format: 'umd',
+    moduleName: 'Toy',
+    plugins
 }
